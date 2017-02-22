d1021a0a0d0485fc22c2237efc62e57b011bcb58
mlopezgalan <lopezgalan.miguel@gmail.com>
2017-02-22 20:58:18 +0100

    Acabado login, trabajando con la muestra de proyectos

---
+++ creacion de proyecto/php/login.proc.php
@@ -0,0 +1,66 @@
+<?php
+	//Obtenemos los datos del formulario
+	extract($_REQUEST);
+	// Desactivar toda notificación de error
+		error_reporting(0);
+	//Los comparamos con la BD
+			//Conexion
+			require_once("conexion.proc.php");
+			//Si nos llega una variable de alumno, buscaremos en Alumno
+			if(isset($matricula))
+			{
+				//SQL
+				$sql = "SELECT * FROM `tbl_alumno` WHERE `matricula_alumno` = ".$matricula." ";
+				//Hacemos la petición a la BD
+				$result	=	mysqli_query($conexion,$sql);
+				//Contamos los resultados que nos devuelve
+				$total  = mysqli_num_rows($result); 
+				//Si existe la matrícula creamos la sesion con su ID
+				if($total==1){
+					session_start();
+					while ($fila = mysqli_fetch_array($result)) 
+					{
+						//Añadimos la matricula del usuario en una variable de session
+						$_SESSION['user']=$fila['matricula_alumno'];
+						header("location:ver_proyectos.php");
+					}
+				}
+				
+				else
+				{
+					
+					echo "<script type='text/javascript'>alert('La matricula no existe');
+						location.href='../../index.html';</script>";
+				}
+			} //END IF MATRICULA
+			//Si nos llega una variable de usuario de profesor
+			if(isset($prof_name))
+			{
+				//SQL
+				$sql = "SELECT * FROM `tbl_profesor` WHERE `usuario_profesor` LIKE '".$prof_name."' AND `password` LIKE '".$prof_pass."' ";
+				//Hacemos la petición a la BD
+				$result	=	mysqli_query($conexion,$sql);
+				//Contamos los resultados que nos devuelve
+				$total  = mysqli_num_rows($result); 
+				//Si nos devuelve un resultado, significa que tanto la contraseña como el usuario son correctos
+				if($total==1){
+					session_start();
+					while ($fila = mysqli_fetch_array($result)) 
+					{
+						//Añadimos el nombre de usuario a la variable de session
+						$_SESSION['user']=$fila['usuario_profesor'];
+							//Comprobamos el tipo de usuario, si el profesor es administrador, lo redirigimos a la pagina de administración
+							if($fila['id_tipo_usuario']==3)//El tres, es tipo administrador
+							{
+								header("location:administracion.php");
+							}
+							//Si es de tipo 2, es profesor normal
+							else if($fila['id_tipo_usuario']==2)
+							{
+								header("location:profesor.php");
+							}
+						//header("location:ver_proyectos.php");
+					}
+				}
+			}
+?>
--- creacion de proyecto/php/ver_proyectos.php
+++ creacion de proyecto/php/ver_proyectos.php
@@ -1,13 +1,13 @@
 <?php
 	require_once("conexion.proc.php");
 	session_start();
-	if(!isset ($_SESSION['alu_matricula']))
+	if(!isset ($_SESSION['user']))
 	{
 		echo "<script type='text/javascript'>alert('No esta registrado');
 					location.href='../../index.html';</script>";
 					die;
 	}
-	$sql="SELECT * FROM `tbl_alumno` WHERE `matricula_alumno` = ".$_SESSION['alu_matricula']."";
+	$sql="SELECT * FROM `tbl_alumno` WHERE `matricula_alumno` = ".$_SESSION['user']."";
 	$result	=	mysqli_query($conexion,$sql);
 	while ($fila = mysqli_fetch_array($result)) 
 				{
@@ -24,34 +24,39 @@
 	   		<link href="../../css/myStyle.css" rel="stylesheet">
 	    <!-- Custom Fonts -->
 	    	<link href="../../css/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	    	<script type="text/javascript">
+	    	//Funcion de confirmacion de logout
+	    	//Devuelve la var confirmación que recoge la respuesta del usuario
+	    	function confirmacion()
+	    	{
+	    		var confirmacion  = confirm("¿Estás seguro que te quieres desloguear?");
+	    		return confirmacion;
+	    	}
+	    	</script>
 </head>
 <body>
-	<div class="container col-lg-10">
-		<div class="header col-lg-10">
-			<div class="user_div_logo col-lg-2">
+	<div class="container col-lg-12">
+		<div class="header col-lg-12">
 				<img src="../../css/image/logo.png" class= "user_logo img-rounded" />
-			</div><!-- END LOGO-->
-			<div class="user_logged col-lg-8">
-				<div class="col-lg-5">
-					<p>
-						<?php echo $name; ?>
-					</p>
-				</div>
-			<div class="user_option col-lg-2">
-				<i class="fa fa-power-off"></i>
-			</div>
-			</div><!-- END USER LOGGED-->
+					<div class="head_text">
+							<?php echo $name; ?>
+							<a href="logout.proc.php" onclick="return confirmacion();"><i class="fa fa-power-off"></i></a>
+					</div><!-- END HEAD_TEXT-->
 		</div><!--END HEADER -->
-	<div class="show_proyects col-lg-8">
+	<div class="show_proyects col-lg-12">
+		
 		<?php
 		 $sql = "SELECT * FROM `tbl_proyecto` ";
 		 $result	=	mysqli_query($conexion,$sql);
 			while ($fila = mysqli_fetch_array($result)) 
 				{
-					echo $fila['titulo_proyecto'] . "<br/>";
+					echo "<div class='proyect  col-lg-6'>";
+						echo $fila['titulo_proyecto'] . "<br/>";
+					echo "</div>";
 				}
 
 		?>
 	</div>
+
 </body>
 </html>
