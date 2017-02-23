90b003103e1eb477fc72d2bf2611486023539daf
MarcPetit92 <1001489.joan23@fje.edu>
2017-02-23 18:41:53 +0100

    subo la fumada!!!! funsiona

--- valoracion tribunal/php/valorar_tribunal.proc.php
+++ valoracion tribunal/php/valorar_tribunal.proc.php
@@ -1,29 +1,52 @@
 <?php
- extract($_REQUEST);
 
-//echo $id_proyecto;
+ extract($_REQUEST);
 
 include("conexion.proc.php");
 
-echo $nota_7138_3;
+//echo $nota_7138_3;
+$notas = array();
+$
+foreach ($notas as $nota ) {
+	echo $nota."<br>";
+}
+foreach ($idpreguntas as $id ) {
+	echo $id."<br>";
+}
+
+foreach ($matriculas as $matric ) {
+	echo $matric."<br>";
+}
+
+//echo $variable;
+
 
 	//tenemos que sacar las notas de cada alumno y cada pregunta. la nomenclatura de las notas es nota_matricula_idpregunta
 
 	$consulta= "SELECT matricula_alumno FROM  tbl_integrante_proyecto WHERE id_proyecto=".$id_proyecto; 
-	echo $consulta."<br>";
+	//echo $consulta."<br>";
 	$resultado= mysqli_query($conexion, $consulta) or die (mysqli_error());
 	while($fila = mysqli_fetch_array($resultado)){	
 			
 
 			$sql= "SELECT id_pregunta_tribunal FROM  tbl_pregunta_tribunal "; 
-			echo $sql."<br>";
+			//echo $sql."<br>";
 			$ids= mysqli_query($conexion, $sql) or die (mysqli_error());
 			while($id_pregunta = mysqli_fetch_array($ids)){	
-					
-				echo $.$fila['matricula_alumno']."_".$id_pregunta['id_pregunta_tribunal']."<br>";
+				
+				//$valor = $($fila['matricula_alumno']."-".$id_pregunta['id_pregunta_tribunal'])."<br>";	
+				array_push($notas,$valoracion);
 				//echo $valoracion;
 			}
 				
+		}
+
+
+
+
+		foreach ($notas as $nota) {
+			//echo $nota."<br>";
+
 		}
 	mysqli_close($conexion);
 	//echo '</select>';
